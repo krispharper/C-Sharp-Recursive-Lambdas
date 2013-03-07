6647eda7f19f5052572f9b264d77405b5039a189
Kris harper <kris.p.kharper@gmail.com>
2013-03-07 12:40:45 -0500

    First commit

---
+++ Recursive Lambdas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test
+{
+	class Program
+	{
+		delegate Func<Targ, TResult> Recursive<Targ, TResult>(Recursive<Targ, TResult> r);
+
+		static void Main(string[] args)
+		{
+			Func<int, int> fib = Y<int, int>(f => n => n > 1 ? f(n - 1) + f(n - 2) : n);
+			Func<int, int> fac = Y<int, int>(f => n => n == 1 ? n : n * f(n - 1));
+		}
+
+		static Func<Targ, TResult> Y<Targ, TResult>(Func<Func<Targ, TResult>, Func<Targ, TResult>> f)
+		{
+			Recursive<Targ, TResult> rec = r => a => f(r(r))(a);
+			return rec(rec);
+		}
+	}
+}
